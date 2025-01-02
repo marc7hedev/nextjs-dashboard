ad127e64856ad70d9844999ceb7a79f1aa1580ad
marc7hedev <driberstagg@gmail.com>
2025-01-02 16:38:43 -0600

    Implementado componenete Image

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,9 @@
-import {Montserrat} from 'next/font/google'
+import {Lusitana, Montserrat} from 'next/font/google'
 
 export const montserrat = Montserrat({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+})
+
