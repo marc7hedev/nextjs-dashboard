839ff59338b7402549e9abbf3388c09d5956f9af
marc7hedev <driberstagg@gmail.com>
2025-01-03 13:46:58 -0600

    Implementada pantalla de espera Skeleton fallback Suspense

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,9 +3,10 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import { Suspense } from "react";
+import { RevenueChartSkeleton } from "../ui/skeletons";
 
 export default async function Page() {
-    const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
 
     return (
@@ -18,13 +19,15 @@ export default async function Page() {
                 {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
                 {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
                 {/* <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
+            title="Total Customers"
+            value={numberOfCustomers}
+            type="customers"
         /> */}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenue} />
+                <Suspense fallback={<RevenueChartSkeleton/>}>
+                    <RevenueChart />
+                </Suspense>
                 <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
